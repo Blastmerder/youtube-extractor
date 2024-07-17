7c344c890234cf17b735eaa15f6d0bdfa466fc7b
Blastmerder <100530405+Blastmerder@users.noreply.github.com>
2024-07-17 17:48:24 +0300

    Add All

--- test.py
+++
@@ -1,8 +0,0 @@
-from search_video import search_video
-import re
-
-name1 = 'Feerix "Chuggy Demon"'
-video_name = 'Feerix - Chuggy Demon'
-name2 = 'Introducing Demon Chuggx-'
-
-print(search_video(name1))
